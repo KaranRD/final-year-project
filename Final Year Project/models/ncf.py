cfe2dfb6ef35ab837ca1268b9ee0dff53437f51c
KaranRD <karanukhu@gmail.com>
2026-02-25 11:29:35 +0530

    Create NCF model class

---
+++ Final Year Project/models/ncf.py
@@ -0,0 +1,17 @@
+class NCF:
+    def __init__(self, num_users, num_items):
+        self.num_users = num_users
+        self.num_items = num_items
+        # Initialize additional necessary parameters here
+
+    def forward(self, user_input, item_input):
+        # Implement forward pass logic here
+        pass
+
+    def train(self, training_data):
+        # Implement training logic here
+        pass
+
+    def predict(self, user_id, item_id):
+        # Implement prediction logic here
+        pass
