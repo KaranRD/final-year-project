d65a4b6bd8e92fd635c0b8d668fa855f8d0368ec
KaranRD <karanukhu@gmail.com>
2026-02-25 11:30:00 +0530

    Add Hybrid model class

---
+++ Final Year Project/models/hybrid.py
@@ -0,0 +1,12 @@
+class Hybrid:
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def fit(self, X, y):
+        # Code for fitting the model
+        pass
+
+    def predict(self, X):
+        # Code for making predictions
+        pass
